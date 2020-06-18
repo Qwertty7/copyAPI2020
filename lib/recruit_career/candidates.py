884b99e6c2685bfb38f7a00bddbe7ebd96df7477
Elena <742042@gmail.com>
2020-06-17 17:37:27 -0700

    added test case: update_candidate_data

--- lib/recruit_career/candidates.py
+++ lib/recruit_career/candidates.py
@@ -12,3 +12,8 @@ class Candidates(BaseClient):
         url = self.base_url + '/candidates'
         print(self.session.headers)
         return self.session.post(url, json=candidate_data)
+
+
+    def update_candidate_data(self, candidate_id, new_data):
+        print(self.session.headers)
+        return self.session.put(self.base_url + '/candidates/' + str(candidate_id), json=new_data)
--- tests/my_test.py
+++ tests/my_test.py
@@ -9,23 +9,22 @@ from lib.recruit_career.rc_client import RecruitClient
 
 
 class YahooAPITestCase(unittest.TestCase):
-    def test_for_successful_response(self):
+    def test_for_successful_response (self):
         result = requests.get("http://www.yahoo.com")
         self.assertEqual(200, result.status_code)
         # OR
         self.assertTrue('OK' == result.reason)
 
 
-
 class CareerPortalTests(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp (self) -> None:
         self.f = Faker()
 
-    def test_login(self):
+    def test_login (self):
         client = RecruitClient()
         client.authentication.authenticate('jane@example.com', 'pass')
 
-        # # if you wanter parallel clients
+        # # if you want parallel clients
         # client2 = RecruitClient()
         # client2.authentication.authenticate('bob@example.com', 'Boo')
         #
@@ -38,31 +37,31 @@ class CareerPortalTests(unittest.TestCase):
 
         #############
 
-        # positions = client.positions.get_all_positions()
-        # json_positions = json.loads(positions.text)
-        #
-        # self.assertIsInstance(json_positions, list)
-        # self.assertGreaterEqual(len(json_positions), 5)
-        #
-        # result = client.authentication.authenticate("student@example.com", "welcome")
-        # self.assertEqual(200, result.status_code)
-        #
-        # json_parsed = json.loads(result.text)
-        # self.assertTrue(json_parsed['authenticated'])
-        #
-        # verify_response = client.authentication.perform_user_verification()
-        # verify_content = json.loads(verify_response.content)
-        #
-        # user_id = verify_content['id']
-        # email = verify_content['email']
-        #
-        # self.assertTrue(email == 'student@example.com')
-        # self.assertEqual(8, user_id)
-        #
-        # my_positions = client.candidate.get_candidate_positions(user_id)
-        # json_my_positions = json.loads(my_positions.text)
-        #
-        # self.assertLessEqual(1, len(json_my_positions))
+        positions = client.positions.get_all_positions()
+        json_positions = json.loads(positions.text)
+
+        self.assertIsInstance(json_positions, list)
+        self.assertGreaterEqual(len(json_positions), 5)
+
+        result = client.authentication.authenticate("student@example.com", "welcome")
+        self.assertEqual(200, result.status_code)
+
+        json_parsed = json.loads(result.text)
+        self.assertTrue(json_parsed['authenticated'])
+
+        verify_response = client.authentication.perform_user_verification()
+        verify_content = json.loads(verify_response.content)
+
+        candidate_id = verify_content['id']
+        email = verify_content['email']
+
+        self.assertTrue(email == 'student@example.com')
+        self.assertEqual(8, candidate_id)
+
+        my_positions = client.candidate.get_candidate_positions(candidate_id)
+        json_my_positions = json.loads(my_positions.text)
+
+        self.assertLessEqual(1, len(json_my_positions))
 
         # """
         # test create new candidate and check that data exist
@@ -87,21 +86,32 @@ class CareerPortalTests(unittest.TestCase):
 
         }
         response = client.candidate.create_new_candidate(candidate_data)
-        # json_response = json.loads(response.text)
-        print(response.content)
+        json_response = json.loads(response.content)
+        print(json_response)
+        self.assertIn('id', json_response)
 
+        candidate_id = json_response['id']
+        candidate_first_name = json_response['firstName']
+        candidate_email = json_response['email']
+        print(candidate_id)
+        new_data = {
+            "firstName": candidate_first_name,
+            "lastName": "NewLastName",
+            "email": candidate_email,
+            "password": "newPASSWORD"
+        }
+        candidate_page = client.candidate.update_candidate_data(candidate_id, new_data)
+        json_candidate_page = json.loads(candidate_page.text)
+        print(json_candidate_page)
 
-        # self.assertEqual(200, response.status_code)
-        # # self.assertIn()
+        self.assertEqual('NewLastName', json_candidate_page['lastName'])
+        # self.assertEqual('newPASSWORD', json_candidate_page['password'])
 
-
-
-
-    def test_cannot_login(self):
-        sess = Authenticate()
-        response = sess.authenticate('foo', 'barr')
-        json_parsed = json.loads(response.text)
-        self.assertEqual('Incorrect email: foo', json_parsed['errorMessage'])
+    # def test_cannot_login(self):
+    #     sess = Authenticate()
+    #     response = sess.authenticate('foo', 'barr')
+    #     json_parsed = json.loads(response.text)
+    #     self.assertEqual('Incorrect email: foo', json_parsed['errorMessage'])
 
 
 if __name__ == '__main__':
